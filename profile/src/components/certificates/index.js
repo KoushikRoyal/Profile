a8ab578e53bca7fc1b29ae38d42ee682ba233c67
koushik <akulakoushik2003@gmail.com>
2024-08-03 10:48:24 +0530

    second commit

---
+++ profile/src/components/certificates/index.js
@@ -0,0 +1,108 @@
+
+import React from 'react'
+import styled from 'styled-components'
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import ExperienceCard from '../Cards/CertificateCard';
+import { certificates } from '../../data/constants';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    z-index: 1;
+    align-items: center;
+    padding: 40px 0px 80px 0px;
+    @media (max-width: 960px) {
+        padding: 0px;
+    }
+`;
+
+const Wrapper = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    max-width: 1350px;
+    padding: 80px 0;
+    gap: 12px;
+    @media (max-width: 960px) {
+        flex-direction: column;
+    }
+`;
+
+const Title = styled.div`
+font-size: 42px;
+text-align: center;
+font-weight: 600;
+margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+      margin-top: 12px;
+      font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+    font-size: 18px;
+    text-align: center;
+    max-width: 600px;
+    color: ${({ theme }) => theme.text_secondary};
+    @media (max-width: 768px) {
+        margin-top: 12px;
+        font-size: 16px;
+    }
+`;
+
+const TimelineSection = styled.div`
+    width: 100%;
+    max-width: 1000px;
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+`;
+
+
+
+const index = () => {
+    return (
+        <Container id="certificates">
+            <Wrapper>
+                <Title>Certificates</Title>
+                <Desc>
+                I have got this skills, and I have got this certificates to prove it !
+                </Desc>
+                <TimelineSection sx={{m:2}} >
+                    <Timeline>
+                        {certificates.map((experiences,index) => (
+                            <>
+                                <>
+                                    <class variant="outlined" color="secondary" >
+                                    {index !== experiences.length - 1 && <TimelineConnector style={{ background: '#854CE6' }} />}
+                                    </class>
+                                </>
+                                <class sx={{ py: '12px', px: 3,margin:'' }}>
+                                    <ExperienceCard experience={experiences}/>
+                                    <br/>
+                                </class>
+                            </>
+                        ))}
+                    </Timeline>
+
+                </TimelineSection>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default index
---
+++ profile/src/data/constants.js
@@ -0,0 +1,334 @@
+import oracle from "../images/oracle.jpg";
+import awscp from "../images/awscp.jpg";
+import awssaa from "../images/awssaa.jpg";
+import lms from "../images/library_management_project.jpg";
+import amms from "../images/automobile_management.jpg";
+import mlhd from "../images/ml_on_heart_disease.jpg";
+import pdp from "../images/potato_disease_prediction.jpg";
+import redhat from "../images/redhat.jpg";
+import narayana from "../images/narayana.jpg";
+import sri_chaitanya from "../images/sri_chaitanya.jpg";
+import ak from "../images/HeroImage.jpg";
+import klu from "../images/klu.png";
+import aiml from "../images/aiml.jpg";
+import developercertificate from "../images/developercertificate.jpg";
+export const Bio = {
+  name: "Akula Koushik",
+  roles: [
+    "Full Stack Developer",
+    "Speaker",
+    "Student",
+    "Programmer",
+  ],
+  description:
+    "I am an enthusiastic and adaptable individual who eagerly embraces new challenges. Fueled by a passion for continuous learning, I am committed to delivering exceptional results. With a positive outlook and a mindset focused on growth, I am poised to make a meaningful impact and achieve significant accomplishments.",
+  github: "https://github.com/KoushikRoyal/",
+  resume:
+    "https://flowcv.com/resume/as2lm9gjss",
+  linkedin: "https://www.linkedin.com/in/koushik-akula-914028226/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      // {
+      //   name: "Redux",
+      //   image:
+      //     "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      // },
+      // {
+      //   name: "Next Js",
+      //   image:
+      //     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      // },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      // {
+      //   name: "Flask",
+      //   image:"https://raw.githubusercontent.com/baludbr/My_Achievements/main/flask.jpg",
+      // },
+      // {
+      //   name: "Django",
+      //   image:"https://raw.githubusercontent.com/baludbr/My_Achievements/main/django.png",
+      // },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Spring boot",
+        image:"https://raw.githubusercontent.com/baludbr/My_Achievements/main/spring.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "C",
+        image:"https://raw.githubusercontent.com/baludbr/My_Achievements/main/c.png"
+      },
+      {
+        name:"Java",
+        image:
+        "https://raw.githubusercontent.com/baludbr/My_Achievements/main/java.png"
+      },
+      {
+        name:"Python",
+        image:
+        "https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/files/python-logo-only.svg"
+      }
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name : "Deep Learning",
+        image: "https://raw.githubusercontent.com/baludbr/My_Achievements/main/ml.png"
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://raw.githubusercontent.com/baludbr/My_Achievements/main/postman.png",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://raw.githubusercontent.com/baludbr/My_Achievements/main/eduskills.jpg",
+    role: "AI&ML Virtual Intern",
+    company: "EduSkills Foundation",
+    date: "December 2022 - March 2023",
+    desc: "Learn the fundamentals of Cloud and AI & ML using AWS Academy",
+    skills: [
+      "Cloud Computing",
+      "Machine Learning",
+      "Artificial Intelligence",
+      "Amazon Web Services",
+    ],
+    doc:aiml,
+  }
+];
+
+export const education = [
+  {    id: 0,    img:klu,
+    school: "KL University,Vijayawada",
+    date: " 09/2021 – Present",
+    grade: "9.64 CGPA(Present)",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at KL University,Vijayawada. I have completed 4 semesters and have a CGPA of 9.88. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a Director of Community of the Kognitiv University at KL University, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img:narayana,
+    school: "Narayana Junior College, Madhapur, Hyderabad",
+    date: "July 2019 - July 2021",
+    grade: "97.6%",
+    desc: "I completed my Intermediate (MPC)  in Narayana Junior College, Hyderabad with an exceptional score of 976/1000. This experience has equipped me with valuable skills and knowledge for the next steps in my educational journey.",
+    degree: "Board of Intermediate Education,Telangana",
+  },
+  {
+    id: 2,
+    img: sri_chaitanya,
+    school: "Sri Chaitanya Techno School,Hyderabad",
+    date: "June 2018 - May 2019",
+    grade: "9.8 CGPA",
+    desc: "I completed my class 10 education at Sri Chaitanya Techno School,Hyderabad with 9.8 CGPA",
+    degree: "Board of Secondary Education,Telangana",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Automobile Management system",
+    date: "May 2023 - Sep 2023",
+    description:" The React-based automobile management system prioritizes security",
+    image:amms,
+    tags: [
+     "React",
+      "Node js",
+      "MongoDB",
+      "Netlify",
+      "Cyclic.sh",
+      "Express Js",
+      "Redux",
+    ],
+    github:"https://github.com/KoushikRoyal/Automoblile-Management-System-MSWD",
+    category: "web app",
+    member: [
+      {
+        name: "Akula Koushik",
+        img: ak,
+        linkedin: "https://www.linkedin.com/in/koushik-akula-914028226/",
+        github:"https://github.com/KoushikRoyal/",
+      }
+    ],
+  },
+  {
+    id: 1,
+    title: "Heart Disease Prediction Using Machine Learning",
+    date: "Jun 2023 - Sep 2023",
+    description:"Using Machine Learning ensemble classifiers like Random forest to improve the accuracy of prediction",
+    image:mlhd,
+    tags: [
+      "Machine Learning"
+    ],
+    category: "ML",
+    github: "https://github.com/KoushikRoyal/Heart-Disease-Prediction-using-Machine-Learning",
+    member: [
+      {
+        name: "Akula Koushik",
+        img: ak,
+        linkedin: "https://www.linkedin.com/in/koushik-akula-914028226/",
+        github:"https://github.com/KoushikRoyal/",
+      }
+    ],
+  },
+  {
+    id: 2,
+    title: "Library Management System ",
+    date: "Jan 2023-Feb 2024",
+    description:"TalentHub is a comprehensive web-based platform designed to facilitate the efficient management of candidate profiles throughout the recruitment process. The system aims to streamline the entire recruitment lifecycle, from the initial submission of candidate profiles to the final selection and onboarding.",
+    image:lms,
+    tags: [
+      "Core Java",
+      "JPA",
+      "JSP",
+      "JSF",
+      "MySQL",
+      "HTML",
+      "CSS",
+      "Javascript",
+      "JDBC"
+    ],
+    category: "web app",
+    github: "https://github.com/KoushikRoyal/LibraryManagement_SpringBoot",
+    member: [
+      {
+        name: "Akula Koushik",
+        img: ak,
+        linkedin: "https://www.linkedin.com/in/koushik-akula-914028226/",
+        github:"https://github.com/KoushikRoyal/",
+      }
+    ]
+  },
+  {
+    id: 9,
+    title: "Image Recognition System",
+    date: "Jun 2023 - Jul 2023",
+    description:"By using tensorflow , i trained the model for large sized datasets and predicted the image which class it belongs in general",
+    image:pdp,
+    tags: [
+      "Deep Learning"
+    ],
+    category: "web app",
+    github:"https://github.com/KoushikRoyal/Visual-search-and-image-recognition-using-deep-learning",
+    member: [
+      {
+        name: "Akula Koushik",
+        img: ak,
+        linkedin: "https://www.linkedin.com/in/koushik-akula-914028226/",
+        github:"https://github.com/KoushikRoyal/",
+      }
+    ],
+  }
+];
+
+
+export const certificates=[
+  {
+    id: 2,
+    img:"https://raw.githubusercontent.com/baludbr/My_Achievements/main/redhat.jpg",
+    role: "Red Hat Certified Enterprise Application Developer",
+    company: "Red Hat",
+    date: " Aug 2023 - Aug 2026",
+    desc: "I am a Red Hat Certified Enterprise Application Developer, proficient in application development, certified in August 2023, and my credential is valid until August 2026",
+    doc: redhat,
+  },
+    {
+      id: 2,
+      img:"https://raw.githubusercontent.com/baludbr/My_Achievements/main/oracle.jpg",
+      role: "Oracle Machine Learning using Autonomous Database 2023 Certified",
+      company: "Oracle",
+      date: " Aug 2023 - Aug 2025",
+      desc: "I am certified in Oracle Machine Learning using Autonomous Database 2023, demonstrating proficiency in leveraging machine learning within Oracle's Autonomous Database, valid until August 2025.",
+      doc: oracle,
+    },
+   
+    {
+      id: 2,
+      img:"https://raw.githubusercontent.com/baludbr/My_Achievements/main/aws.jpg",
+      role: "AWS Solution Architect",
+      company: "Amazon Web Services(AWS)",
+      date: " Nov 2023 - Nov 2026",
+      desc: "I am an AWS Solution Architect with expertise in designing and implementing scalable and efficient cloud solutions",
+      doc: awssaa,
+    },
+
+    {
+      id: 2,
+      img:"https://images.datacamp.com/image/upload/v1703239056/image2_c905c5b7d7.png",
+      role: "Tensorflow Developer Certificate",
+      company: "Google",
+      date: " Apr 2024 - Apr 2027",
+      desc: "Building and training neural network models using TensorFlow, Image classification, Natural language processing (NLP), Time series, sequences and predictions",
+      doc: developercertificate,
+    },
+    {
+    id: 2,
+    img:"https://raw.githubusercontent.com/baludbr/My_Achievements/main/aws.jpg",
+    role: "AWS Certified Cloud Practitioner",
+    company: "Amazon Web Services(AWS)",
+    date: " Jun 2023 - Jun 2026",
+    desc: "I am an AWS Certified Cloud Practitioner, officially recognized by Amazon Web Services (AWS). I obtained this certification in June 2023, and it remains valid until June 2026.",
+    doc: awscp,
+  }
+]
